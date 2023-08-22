0e4ffb4bc50d4d9e581f731bd62a3de025919c63
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-22 14:58:48 -0500

    create register and login

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../components/home/home';
+import Login from '../components/login/Login';
+import Register from '../components/register/Register';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
