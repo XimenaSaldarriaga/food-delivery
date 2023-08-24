a9a4d479da229d7f59c6c0f5ad029458bea1663a
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-24 09:30:20 -0500

    create home

---
+++ src/components/location/Location.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Location = () => {
+  return (
+    <div>
+        <div>
+            <p>Allow access to geo data on the device mark your address on the map</p>
+        </div>
+        <div>
+            <button>Confirm</button>
+        </div>
+    </div>
+  )
+}
+
+export default Location
