fedb1779c235caeff14d30436993ba3d8e72b377
Neis Rosado <neisrosadom@gmail.com>
2023-08-26 01:53:13 -0500

    Add return route to restaurant page

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import './product.scss'
 import back from '../../assets/Back.png';
 import time from '../../assets/Time.png';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 const Product = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const restaurantId = searchParams.get('restaurantId');
+    const restaurantName = searchParams.get('restaurantName'); 
+
+    const handleBackClick = () => {
+        if (restaurantId && restaurantName) {
+            navigate(`/restaurant/${restaurantId}`, { state: { restaurantName } });
+        } else {
+            navigate(`/`); 
+        }
+    };
+
 
 
     return (
         <div className='product'>
             <div className='relative'>
-                <img className='absolute left-8 top-8' src={back} alt="" />
+                <img
+                    className='absolute left-8 top-8 cursor-pointer'
+                    src={back}
+                    alt=""
+                    onClick={handleBackClick}
+                />
                 <img className='rounded-md w-[100%] object-contain' src="https://s3-alpha-sig.figma.com/img/255f/ec55/df09594f35ff96fcae46c638467e42c8?Expires=1693785600&Signature=bp9g0oXJ9KM1E3Ku428-VAaMEg2qxcnWN0WITXYX4jL5uck1MjpoEzWqe8xKFLVY075ez7~dRjdGAYVZ7G~Shkc2XMgUWGdO8WrvUluS5m9aA9gXxw33-hOJA-WAgyM~W2ndxPkKnr2E-CLoRFrMCHBiR1AqNY0UGZoRv~VuQG6P6t9U-P2Uo~fBcraCMwcv8AvejkyB8UwAXL9L2xOgCj4-CeKDG2PikkE85BP5wkBulR1f7-yjBPt2b0GIx3xdwQSOCAKDE4VDhATylq2Zdo92dD23kTBZWB6a6IEh34-W3axJZ~7D51bxXFX8~bhXKr2g0NMDV0Abt00h6BndMA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
             </div>
             <div className=' flex flex-col gap-[6rem] m-4'>
