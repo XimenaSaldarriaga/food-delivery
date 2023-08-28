7e30c816b4ed2e0873b9daa6f77deb5c42326c89
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-28 13:45:55 -0500

    add render to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,14 +3,34 @@ import home from '../../assets/Home.png';
 import search from '../../assets/Search.png';
 import orders from '../../assets/Orders.png';
 import profile from '../../assets/Profile.png';
+import { useNavigate } from 'react-router-dom'
+
 
 const Footer = () => {
+
+    const navigate = useNavigate();
+
+    const goToHome= () => {
+       navigate('/home')
+    }
+    const goToSearch= () => {
+        navigate('/search')
+     }
+     
+     const goToOrders= () => {
+        navigate('/orders')
+     }
+     const goToProfile= () => {
+        navigate('/profile')
+     }
+
+
     return (
         <div className='bg-white flex justify-between fixed bottom-0 left-0 p-4 w-full'>
-            <img className='object-contain' src={home} alt="" />
-            <img className='object-contain' src={search} alt="" />
-            <img className='object-contain' src={orders} alt="" />
-            <img className='object-contain' src={profile} alt="" />
+            <img className='object-contain' onClick={goToHome} src={home} alt="" />
+            <img className='object-contain' onClick={goToSearch} src={search} alt="" />
+            <img className='object-contain' onClick={goToOrders} src={orders} alt="" />
+            <img className='object-contain' onClick={goToProfile} src={profile} alt="" />
         </div>
     )
 }
