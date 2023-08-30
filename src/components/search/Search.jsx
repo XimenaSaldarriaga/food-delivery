724849039b6ddd9665452e124e12ca7c7c4e6eee
ximena saldarriaga <xime-2208@hotmail.com>
2023-08-30 14:52:06 -0500

    create search function

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import search from '../../assets/SearchGray.png';
-import './search.scss'
+import './search.scss';
 import Footer from '../footer/Footer';
+import { useAuth } from '../../context/authContext';
 
 const Search = () => {
+  const { fetchAllMenus } = useAuth();
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const allMenus = await fetchAllMenus();
+      const matchingDishes = [];
+
+      allMenus.forEach(({ menuData }) => {
+        const matchingDishesInMenu = menuData.filter(dish =>
+          dish.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        matchingDishes.push(...matchingDishesInMenu);
+      });
+
+      setSearchResults(matchingDishes);
+    } catch (error) {
+      console.error('Error searching for dishes:', error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    const newSearchQuery = event.target.value;
+    setSearchQuery(newSearchQuery);
+    handleSearch();
+  };
+
   return (
     <div className='search flex flex-col gap-8 my-10 mx-5 text-[14px]'>
       <div className='relative'>
         <input
-          className='bg-gray-100 w-full rounded-md px-5 py-2 text-[14px] outline-none'
-          type="text"
+          className='bg-gray-100 w-full rounded-md px-8 py-2 text-[14px] outline-none'
+          type='text'
           placeholder='   Search for a dish'
+          value={searchQuery}
+          onChange={handleInputChange}
         />
         <img
           src={search}
@@ -19,17 +51,21 @@ const Search = () => {
         />
       </div>
 
-      <div className='flex gap-4 items-center'>
-        <img className='w-12 rounded-md' src="https://inquiringchef.com/wp-content/uploads/2020/05/Greek-Pizza_square.jpg" alt="" />
-        <div className='flex flex-col gap-1'>
-          <span className='font-semibold'>Meat pizza</span>
-          <span className='text-gray-400'>$ 29.00</span>
+      {searchResults.map(dish => (
+        <div className='flex gap-4 items-center' key={dish.id}>
+          <img className='w-20 rounded-md' src={dish.image} alt='' />
+          <div className='flex flex-col'>
+            <span className='font-semibold'>{dish.name}</span>
+            <span className='text-gray-400'>{dish.price}</span>
+            <span className='text-gray-400'>{dish.time}</span>
+          </div>
         </div>
-      </div>
+      ))}
 
       <Footer />
     </div>
   );
-}
+};
 
 export default Search;
+
