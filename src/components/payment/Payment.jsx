f720092b946f54b75e629963180c270d7acb9071
ximena saldarriaga <xime-2208@hotmail.com>
2023-09-02 10:47:41 -0500

    update payment

--- src/components/payment/Payment.jsx
+++ src/components/payment/Payment.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import master from '../../assets/master.png';
+import visa from '../../assets/visa.png';
 import back from '../../assets/back.png';
-import eye from '../../assets/eye.png';
+import del from '../../assets/delete.png';
 import { useNavigate } from 'react-router-dom';
 import { useAuth, getCardsForUserByEmail } from '../../context/authContext';
 import './payment.scss'
@@ -9,14 +10,14 @@ import './payment.scss'
 const Payment = () => {
   const { userData } = useAuth();
   const navigate = useNavigate();
-  const [cardData, setCardData] = useState(null);
+  const [cardData, setCardData] = useState([]);
 
   useEffect(() => {
     if (userData) {
       const userEmail = userData.email;
       getCardsForUserByEmail(userEmail)
         .then((cards) => {
-          setCardData(cards[0]);
+          setCardData(cards);
         })
         .catch((error) => {
           console.error('Error fetching card data:', error);
@@ -36,15 +37,27 @@ const Payment = () => {
     <div className='payment flex flex-col gap-16 relative mx-6 my-10'>
       <img className='object-contain w-2 absolute top-1 left-2' src={back} alt='' onClick={goProfile} />
       <h1 className='self-center font-semibold'>Payment method</h1>
-      <div className='flex flex-col gap-6'>
-        <div className='flex justify-between'>
-          <div className='flex gap-4'>
-            <img className='object-contain' src={master} alt='' />
-            {cardData ? <span>{cardData.cardNumber}</span> : 'No card added'}
-          </div>
-          <img className='object-contain' src={eye} alt='' />
+
+      {cardData.length > 0 ? (
+        <div>
+          {cardData.map((card, index) => (
+            <div className='flex justify-between' key={index}>
+              <div className='flex gap-4 items-center'>
+                {card.cardName.toLowerCase() === 'visa' && (
+                  <img className='w-10' src={visa} alt='Visa' />
+                )}
+                {card.cardName.toLowerCase() === 'mastercard' && (
+                  <img className='object-contain' src={master} alt='Mastercard' />
+                )}
+                <span>{card.cardNumber}</span>
+              </div>
+              <img className='object-contain w-4' src={del} alt='Eye' />
+            </div>
+          ))}
         </div>
-      </div>
+      ) : (
+        <span>No cards added</span>
+      )}
 
       <button onClick={goAddCard} className='payment__button bg-yellow-300 py-1 rounded-md font-semibold'>
         Add a new card
